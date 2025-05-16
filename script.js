da59809da81ab3ee2a3738f23bfe07e59772f72d
Ryan Holt <ryholtan51@gmail.com>
2025-05-15 18:50:20 -0600

    changed about us image, added links and legals in footer

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.getElementById('hamburger').addEventListener('click', () => {
   document.getElementById('nav-links').classList.toggle('active');
 });
+
+document.getElementById('year').textContent = new Date().getFullYear();
